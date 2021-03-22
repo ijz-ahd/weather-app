7157d756a84cc1cceb29353f18cfa2efabb59962
ijz-ahd <ijazahamed1612@gmail.com>
2021-03-23 00:32:34 +0530

    Initial commit and all set

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+
+const apiKey = "11146cb8c492d7f53b91f62c32958f0a";
+const baseURL = "https://api.openweathermap.org/data/2.5/";
 
 function App() {
+  const [query, setQuery] = useState("");
+  const [weather, setWeather] = useState({});
+
+  const search = (e) => {
+    if (e.key === "Enter") {
+      fetch(`${baseURL}weather?q=${query}&units=metric&APPID=${apiKey}`)
+        .then((res) => res.json())
+        .then((result) => setWeather(result));
+      console.log(weather);
+      setQuery("");
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={`${weather?.main?.temp > 18 ? "app warm" : "app"} app`}>
+      <div className="main">
+        <div className="search_box">
+          <input
+            type="text"
+            className="search_bar"
+            placeholder="Search for weather"
+            value={query}
+            onChange={(e) => {
+              setQuery(e.target.value);
+            }}
+            onKeyPress={search}
+          />
+        </div>
+        {typeof weather.main != "undefined" ? (
+          <>
+            <div className="location_box">
+              <div className="location">
+                {weather.name}, {weather.sys.country}
+              </div>
+              <div className="date">{new Date().toDateString()}</div>
+            </div>
+            <div className="weather_box">
+              <div className="temp">{`${weather.main.temp}°c`}</div>
+              <div className="weather">{weather.weather[0].main}</div>
+            </div>
+          </>
+        ) : (
+          ""
+        )}
+      </div>
     </div>
   );
 }
